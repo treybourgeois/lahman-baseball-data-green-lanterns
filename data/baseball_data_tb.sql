378175ab153b9edb2d9abf05bdd8014adbd43037
treybourgeois <trey@retroactivepedals.com>
2021-03-06 13:48:40 -0600

    first push 3/6

--- data/baseball_data_tb.sql
+++ data/baseball_data_tb.sql
@@ -1,1 +1,13 @@
-select
+/*SELECT COUNT (DISTINCT year)
+FROM homegames; -- 1.) 146 Years
+
+SELECT MIN(year) AS first_year, MAX(year) AS last_year
+FROM homegames; -- 1.) 1871 - 2016 */ -- QUESTION 1
+
+SELECT namefirst, namelast, height
+FROM people
+ORDER BY height ASC; -- Eddie Gradel is shortest player -- Height = 43
+
+
+SELECT span_first, span_last
+FROM homegames;
