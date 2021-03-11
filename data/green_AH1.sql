833ad3348a9dabb36532cc3ff9872ca586b65ed2
Abdullah0930 <Abdullahissack@outlook.com>
2021-03-11 15:41:29 -0600

    Q 5,6

--- data/green_AH1.sql
+++ data/green_AH1.sql
@@ -2,7 +2,7 @@ SELECT *
 FROM batting;
 
 --Question 5. 
-/*SELECT CASE 
+SELECT CASE 
             WHEN yearid between 1920 AND 1929 then '1920s'
             WHEN yearid between 1930 AND 1939 then '1930s'
 			WHEN yearid between 1940 AND 1949 then '1940s'
@@ -20,16 +20,26 @@ FROM batting;
 
 FROM 
 (SELECT yearid, so AS strike_out, hr as home_run
-FROM batting) Sub
+FROM batting) b
 GROUP BY decades
-ORDER BY decades */ ---- strikeouts and homeruns seem to have linear relationship
+ORDER BY decades -- strikeouts and homeruns seem to have linear relationship
 
 
---Q.6- Find by percentage if need be
+--Question.6
+SELECT b.playerid,p.namefirst,p.namelast, 
+SUM(sb) AS stolen_base,SUM(CS) AS caught_stealing,
+SUM(sb) + SUM(cs) AS stolen_base_attempts,
+ROUND(SUM(sb)::decimal/(SUM(sb)::decimal+SUM(cs)::decimal),2) AS stolen_base_success_rate
+FROM
+ (SELECT DISTINCT playerid,sb,cs,(SUM(SB)+SUM(CS)) AS sb_attempt
+FROM batting 
+WHERE yearid=2016
+GROUP BY playerid,sb,cs
+HAVING (SUM(SB)+SUM(CS))!=0) b
+INNER JOIN people AS p USING(playerid)
+WHERE sb_attempt>=20
+GROUP BY b.playerid,p.namefirst,p.namelast
+ORDER BY stolen_base_success_rate DESC
+----Chris Owings had the highest success rate .91 in 2016
 
-SELECT playerid, (SUM(SB)*100)/SUM((SB + CS)  AS stolenbase
-FROM batting
-WHERE yearid = '2016' 
-GROUP BY playerid 
-HAVING SUM(SB + CS) >= 20
-ORDER BY stolenbase DESC;
+
